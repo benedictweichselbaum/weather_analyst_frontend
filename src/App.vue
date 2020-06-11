3d4f5461d129cad6c94d625993e419e9ec41f05d
i18043 <i18043>
2020-06-11 20:34:56 +0200

    #master Make first two components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WeatherHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherHeader from './components/WeatherHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WeatherHeader
   }
 }
 </script>
---
+++ src/components/WeatherHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="weatherAppHeader" class="jumbotron text-center">
+      <h1>
+          Wetter-Analyst
+      </h1>
+      <h5>
+          von Benedict Weichselbaum
+      </h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #weatherAppHeader {
+        background-color: #dddddd;
+        margin-top: 0;
+    }
+</style>
